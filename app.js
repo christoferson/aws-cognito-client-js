8f517d74cdd2c3d62203bbb8af385a0ab5c9b6ec
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-11-26 14:29:39 +0900

    Add Login Page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
 
 const mymodule = require('./modules/mymodule');
 const mycognito = require('./modules/mycognito');
@@ -10,10 +11,24 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!' + mymodule.myDateTime());
 })
+
+app.get('/cognito/login', function(req, res) {
+  res.sendFile(path.join(__dirname+'/html/login.html'));
+});
+
+app.post('/cognito/login', function(req, res) {
+  console.log(req.body.uname);
+  const data = {
+    "Name": req.body.uname,
+    "Password": req.body.upwd
+  }
+    res.send('Got Parameters!' + mymodule.myDateTime() + " " + JSON.stringify(data));
+});
 
 app.get('/cognito', function(req, res) {
     res.send('Hello World!' + mycognito.cognitoSignIn());
